4f3c99eeb5b5157f54a296eef4c6ff1ca2255d5c
kobayashian <systemdisgood@gmail.com>
2023-08-28 00:30:55 +0300

    heh

--- app/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-#some app components for init
-from app import views
--- run.py
+++
@@ -1,5 +0,0 @@
-#!flask/bin/python
-#import webbrowser
-#webbrowser.open('http://127.0.0.1:5000', new=1)
-from app import app
-app.run(debug = True)
